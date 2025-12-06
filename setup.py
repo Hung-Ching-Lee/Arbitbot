944c7761010a6853c546f7aab0b373956d6a10c4
Arbitbot <test@example.com>
2025-12-06 21:33:59 +0800

    Release v0.1.1 - Simplified package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arbitbot",
-    version="0.1.0",
+    version="0.1.1",
     author="Hung-Ching-Lee",
     description="Simple multi-exchange crypto arbitrage detector",
     long_description=open("README.md").read(),
